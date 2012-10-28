c59a4c23659e419ccf2b265ff3b7f5813662faee
Steven Angles <anglessw@muohio.edu>
2012-10-28 12:08:27 -0400

    Partially implemented search method

--- anglesswStarbucks.cpp
+++ anglesswStarbucks.cpp
@@ -1,5 +1,8 @@
 #include "anglesswStarbucks.h"
+#include <ctime>
+#include <cstdlib>
 
+static const double difference = 0.00001;
 
 anglesswStarbucks::anglesswStarbucks(void)
 {
@@ -7,10 +10,24 @@ anglesswStarbucks::anglesswStarbucks(void)
 
 void anglesswStarbucks::build(Entry* c, int n)
 {
+	srand((unsigned) time(0));
+	length = n;
+	entry_arr_ = new Entry[length];
+	Node* root = new Node(entry_arr_[rand()%length]);
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<i; j++) {
+			if((j+1==i) && (abs(c[j].x-c[i].x) > difference) &&
+				(abs(c[j].y-c[i].y) > difference)) {
+				root->insert(c[i], root, true);
+			}
+		}
+	}
+	delete root;
 }
 
 Entry* anglesswStarbucks::getNearest(double x, double y)
 {
+	return new Entry();
 }
 
 anglesswStarbucks::~anglesswStarbucks(void)
--- anglesswStarbucks.h
+++ anglesswStarbucks.h
@@ -7,21 +7,50 @@ public:
 	Node* right;
 	Node(Entry e) { right = left = NULL; entry = e; }
 	Entry entry;
-	Node* insert(Entry e, Node* r, bool x_level) {
+	Node* insert(Entry e, Node* r, bool x_level)
+	{
 		if(r == NULL) return new Node(e);
 		if(e.x == r->entry.x || e.y == r->entry.y) return r;
 		if(x_level) {
 			if(e.x < r->entry.x) 
-				r->left = insert(e, r->left, !x_level);
+				return r->left = insert(e, r->left, !x_level);
 			else 
-				r->right = insert(e, r->right, !x_level);
+				return r->right = insert(e, r->right, !x_level);
 		} else {
 			if(e.y < r->entry.y) 
-				r->left = insert(e, r->left, !x_level);
+				return r->left = insert(e, r->left, !x_level);
 			else 
-				r->right = insert(e, r->right, !x_level);
+				return r->right = insert(e, r->right, !x_level);
 		}
 	}
+	Node* search(Entry e, Node* r, bool x_level)
+	{
+		if(r == NULL) return NULL;
+		if(x_level) {
+			if(e.x == r->entry.x)
+				return r;
+			else if(e.x < r->entry.x) {
+				Node* candidate;
+				return candidate = search(e, r->left, !x_level);
+			} else {
+				Node* candidate;
+				return candidate = search(e, r->right, !x_level);
+			}
+		} else {
+			if(e.y == r->entry.y)
+				return r;
+			else if(e.y < r->entry.y) {
+				Node* candidate;
+				return candidate = search(e, r->left, !x_level);
+			} else {
+				Node* candidate;
+				return candidate = search(e, r->right, !x_level);
+			}
+		}
+	}
+			
+
+	virtual ~Node() { delete left; delete right; }
 };
 
 class anglesswStarbucks : public Starbucks {
@@ -30,5 +59,8 @@ public:
 	void build(Entry* c, int n);
 	Entry* getNearest(double x, double y);
 	virtual ~anglesswStarbucks(void);
+private:
+	Entry* entry_arr_;
+	int length;
 };
 
